ef96a2899a6e31058232d0bbc4d6688dd7a498c4
MrUmmataliyev <mr.umataliev.1817@gmail.com>
2024-05-14 08:52:40 +0500

    Examples

--- FindArea/Program.cs
+++ FindArea/Program.cs
@@ -1,5 +1,16 @@
 ﻿using FindArea.Library.Algo;
 
-var a = new Triangle(3.12, 5.35, 6);
-var b =a.GetArea();
-Console.WriteLine(b);
+var triangle = new Triangle(3.12, 5.35, 6);
+var t_area =triangle.GetArea();
+Console.WriteLine("Triangle");
+Console.WriteLine(t_area);
+
+var circle = new Circle(3);
+var c_area = circle.GetArea();
+Console.WriteLine("Circle");
+Console.WriteLine(c_area);
+
+var rectangle = new Rectangle(3.12, 5.35);
+var r_areea = rectangle.GetArea();
+Console.WriteLine("Rectangle");
+Console.WriteLine(r_areea);
